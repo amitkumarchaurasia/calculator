350795a21738850a696d0951718e1762f5171163
amitkumarchaurasia <149289990+amitkumarchaurasia@users.noreply.github.com>
2023-11-03 21:20:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = displayValue;
+}
+
+function calculateResult() {
+    try {
+        displayValue = eval(displayValue).toString();
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        displayValue = 'Error';
+        document.getElementById('display').value = displayValue;
+    }
+}
+
+
+
+
